c0e7d960819e16bf0fd06e426e6fe58c6400c3ed
Eduardo Miranda <eduardokira08@gmail.com>
2023-09-04 03:06:27 -0300

    Added html view, initiate dinamic map

--- simulator/dinamic_map.py
+++ simulator/dinamic_map.py
@@ -1,8 +1,13 @@
 
 from copy import deepcopy
 from math import exp 
+from PIL import Image
+from PIL import ImageDraw
+from os import mkdir
+from os.path import isdir 
 
 from constants import Constants
+
 
 class DinamicMap(object):
     """Responsable to control the individual's tracing in the map.
@@ -50,24 +55,79 @@ class DinamicMap(object):
                 self.map[i].append(0)
 
     def draw_map(self, directory, iteration):
+        """Draw the crowd map using the structe map and the individuals colors.
 
-        return
+        Parameters
+        ----------
+        directory : str
+            Contain the directory that the image will be saved.
 
+        iteration : int
+            Number of the iteration.
+        """
+        if not isdir(directory + "/dinamic_map"):
+            mkdir(directory + "/dinamic_map")
 
-    def difusion_decay(self):
+        directory += "/dinamic_map"
+
+        field_size = 20
+        image = Image.new("RGB", (field_size * self.len_col, field_size * self.len_row), Constants.C_WHITE)
+        draw = ImageDraw.Draw(image)
+
+        for i in range(self.len_row):
+            for j in range(self.len_col):
+                if (self.structure_map.map[i][j] == Constants.M_WALL):
+                    draw.rectangle((j * field_size, i * field_size, (j + 1) * field_size, (i + 1) * field_size), Constants.C_BLACK, Constants.C_BLACK)
+                elif (self.structure_map.map[i][j] == Constants.M_OBJECT):
+                    draw.rectangle((j * field_size, i * field_size, (j + 1) * field_size, (i + 1) * field_size), Constants.C_GRAY, Constants.C_GRAY)
+                elif (self.structure_map.map[i][j] == Constants.M_VOID):
+                    draw.rectangle((j * field_size, i * field_size, (j + 1) * field_size, (i + 1) * field_size), Constants.C_LIGHT_BLACK, Constants.C_LIGHT_BLACK)
+                elif (self.structure_map.map[i][j] == Constants.M_DOOR):
+                    draw.rectangle((j * field_size, i * field_size, (j + 1) * field_size, (i + 1) * field_size), Constants.C_RED, Constants.C_BLACK)
+                else:
+                    draw.rectangle((j * field_size, i * field_size, (j + 1) * field_size, (i + 1) * field_size), (255, 255 - 20 * int(self.map[i][j]), 255), Constants.C_BLACK)
+                    if self.map[i][j] != 0:
+                        print(self.map[i][j])
+        
+        #image_name = directory + "/" + self.label + "_dinamic_map_" + str(iteration) + ".png"
+        image_name = directory + "/" + "dinamic_map_" + str(iteration) + ".png"
+        image.save(image_name)
+
+    def difusion_decay3(self):
+        # aux_map = deepcopy(self.map)
+        for i in range(1, len(self.map) - 1):
+            for j in range(1, len(self.map) - 1):
+                # aux_map[i][j] = self.map[i][j] + Constants.DIFUSIONDECAY_ALFA / 4 * (self.map[i+1][j] + self.map[i-1][j] + self.map[i][j+1] + self.map[i][j-1] + 
+                                                                            #    self.map[i-1][j-1] + self.map[i-1][j+1] + self.map[i+1][j-1] + self.map[i+1][j+1])
+                self.map[i][j] = self.map[i][j] - (Constants.DIFUSIONDECAY_SIGMA * self.map[i][j])
+                # if aux_map[i][j] != 0:
+                #     print(f"{aux_map[i][j]},")
+
+            # print('\n')
+
+        # self.map = aux_map
+
+    def difusion_decay2(self):
         aux_map = deepcopy(self.map)
-        for i in range(1, len(self.map)):
-            for j in range(1, len(self.map)):
+        for i in range(1, len(self.map) - 1):
+            for j in range(1, len(self.map) - 1):
                 aux_map[i][j] = ((1 - Constants.DIFUSIONDECAY_ALFA) * (1 - Constants.DIFUSIONDECAY_SIGMA) *
                     self.map[i][j] + Constants.DIFUSIONDECAY_ALFA  * (1 - Constants.DIFUSIONDECAY_SIGMA) / 8 *
                     (aux_map[i + 1][j] + aux_map[i][j + 1] + aux_map[i - 1][j] + aux_map[i][j - 1] +
                     aux_map[i + 1][j + 1] + aux_map[i + 1][j - 1] + aux_map[i - 1][j + 1] + aux_map[i - 1][j - 1]))
-        return
+        self.map = aux_map
 
+    def difusion_decay(self):
+        for i in range(1, len(self.map) - 1):
+            for j in range(1, len(self.map) - 1):
+                self.map[i][j] = (self.map[i][j] 
+                            + Constants.DIFUSIONDECAY_ALFA / 8 *
+                              (self.map[i+1][j] + self.map[i-1][j] + self.map[i][j+1] + self.map[i][j-1] + 
+                               self.map[i-1][j-1] + self.map[i-1][j+1] + self.map[i+1][j-1] + self.map[i+1][j+1])
+                            - self.map[i][j] * Constants.DIFUSIONDECAY_SIGMA)
 
     def calc_dinamic_value(self, row, col, individual_KW):
-        return 1
-        # return exp(individual_KW * self.map[row][col])
+        return exp(individual_KW * self.map[row][col])
 
     # def difusaoDecaimento(self, oldMapa, newMapa):
     #     for i in range(1, newMapa.__len__()-1):
